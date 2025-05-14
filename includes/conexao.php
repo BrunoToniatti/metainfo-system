6613521a992e2339d2670aec0b346fc83ca8dd6d
Bruno Toniatti <toniattibruno08@gmail.com>
2025-05-14 19:55:23 -0300

    feat: Transformando o sistema em funcional

---
+++ includes/conexao.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "localhost";
+$user = "root";
+$password = "";
+$database = "metainfo";
+
+$conn = mysqli_connect($servername, $user, $password, $database);
+
+// if ($conn){
+//     echo "Conectado";
+// }else{
+//     echo "Errou";
+// }
+?>
